38435e78204f11af863fae9fea038b295cd92115
KeitaTK <keita17101042@gmail.com>
2025-09-29 01:17:49 +0900

    出力先フォルダを 'train' から 'runs/train' に変更

--- Custom_training/Annotation/quadcopter/train_quadcopter.py
+++ Custom_training/Annotation/quadcopter/train_quadcopter.py
@@ -11,7 +11,7 @@ def main():
         imgsz=640,                 # 短辺を640pxにリサイズ
         batch=16,                  # バッチサイズ（CPU: Core i7-13700KF, RAM 48GB）
         device='cpu',              # CPU学習
-        project='train',      # 出力先フォルダ
+        project='runs/train',      # 出力先フォルダ
         name='quad_nano_cpu_30ep'  # 実験名
     )
 
